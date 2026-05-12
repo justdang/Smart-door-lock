fd4f64aeb8c1cd22f415ae3f33af7704b6c5c8f6
ThanhNguyen <nguyentienthanh2004bn@gmail.com>
2026-05-12 16:38:20 +0700

    add file system_config.h

---
+++ driver/system_config.h
@@ -0,0 +1,25 @@
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+// --- CẤU HÌNH SERVO ---
+#define PIN_SERVO           18
+#define SERVO_ANGLE_OPEN    90
+#define SERVO_ANGLE_CLOSE   0
+
+// --- CẤU HÌNH KEYPAD 4x4 ---
+#define PIN_KEYPAD_R1 19
+#define PIN_KEYPAD_R2 21
+#define PIN_KEYPAD_R3 22
+#define PIN_KEYPAD_R4 23
+#define PIN_KEYPAD_C1 25
+#define PIN_KEYPAD_C2 26
+#define PIN_KEYPAD_C3 27
+#define PIN_KEYPAD_C4 32
+
+// --- CẤU HÌNH RFID (SPI) ---
+#define PIN_RFID_MISO 12
+#define PIN_RFID_MOSI 13
+#define PIN_RFID_CLK  14
+#define PIN_RFID_CS   15
+
+#endif // SYSTEM_CONFIG_H
